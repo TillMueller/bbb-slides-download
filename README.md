0336af1e19bb57a8b8c3dd03d76f3e06f00f9293
Till Müller <tillhendrikmueller@gmail.com>
2021-02-04 14:54:52 +0100

    Additional functionality

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ The link can be retrieved by "inspecting" the current slide.
 It should look somewhat like this: `https://[BBB-Instance]/bigbluebutton/presentation/9405d6b79322aa96ddcaa2883f54f54ffb5140b5-1612443184585/9405d6b79322aa96ddcaa2883f54f54ea2a140b5-89124431845ef/893b709d78182d3194e1d070e48927d6c68067fc-71f2443237937/svg/22`
 
 Then just run the script:
-`./download-slides.sh [LINK]`
+`./download-slides.sh link`
+
+By default the scripts generates a random string of characters to use as a
+prefix for the file names.
+If you want to specify a prefix you can do this with the second argument:
+`./download_slides.sh link [slide deck name]`
--- download_slides.sh
+++ download_slides.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "USAGE: $0 [LINK]"
+if [ "$#" -lt 1 ] && [ "$#" -gt 2 ]; then
+    echo "USAGE: $0 link [slide deck name]"
     exit
 fi
-BASE_LINK=$1
+BASE_LINK=$(sed 's|\(.*\)/.*|\1|' <<< $1)
 MAX_SLIDES=1024
 
-UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+if [ "$#" -eq 2 ]; then 
+    BASE_NAME=$2
+else
+    BASE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+fi
+
+echo "Downloading slide deck from BBB, output file names will start with \"$BASE_NAME\""
+
 for i in $(seq 1 $MAX_SLIDES); do
 	echo "Downloading slide $i"
 	curl "$BASE_LINK$i" \
+		--silent \
 		--compressed \
 		--fail \
-		--output "${UUID}_$i.svg"
+		--output "${BASE_NAME}_$i.svg" 
 	if [[ $? -ne 0 ]]; then
-	    echo "Done, last slide was $i"
+	    echo "Slide $i could not be found, assuming slide deck ended"
 	    exit
 	fi
 done
