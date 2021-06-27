b2b8e7ea2fe54fb7a33b6deda7f2c17ac6d9af68
Till Müller <tillhendrikmueller@gmail.com>
2021-06-27 16:59:13 +0200

    Error logic fix

--- download_slides.sh
+++ download_slides.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ] && [ "$#" -gt 2 ]; then
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
     echo "USAGE: $0 link [slide deck name]"
+	echo "Found $# parameters"
     exit
 fi
 BASE_LINK=$(sed 's|\(.*\)/.*|\1|' <<< $1)
