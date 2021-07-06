faa0fbdc685561f49bc42225dcd2ae73eb28754e
Till Müller <tillhendrikmueller@gmail.com>
2021-07-06 10:09:44 +0200

    Some code cleanup

--- download_slides.sh
+++ download_slides.sh
@@ -2,15 +2,16 @@
 
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
     echo "USAGE: $0 link [slide deck name]"
-	echo "Found $# parameters"
+    echo "Found $# parameters"
     exit
 fi
+
 BASE_LINK=$(sed 's|\(.*\)/.*|\1|' <<< $1)
 MAX_SLIDES=1024
 
 RANDOM_BASE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-if [ "$#" -eq 2 ]; then 
+if [ "$#" -eq 2 ]; then
     BASE_NAME=$2
 else
     BASE_NAME=$RANDOM_BASE_NAME
@@ -22,20 +23,21 @@ if [[ -d "$RANDOM_BASE_NAME" ]]; then
     echo "Output folder \"$RANDOM_BASE_NAME\" already exists, exiting."
     exit
 fi
+
 mkdir $RANDOM_BASE_NAME || exit
 cd $RANDOM_BASE_NAME
 
 for i in $(seq 1 $MAX_SLIDES); do
-	echo "Downloading slide $i"
-	curl "$BASE_LINK/$i" \
-		--silent \
-		--compressed \
-		--fail \
-		--output "${RANDOM_BASE_NAME}_$i.svg"
-	if [[ $? -ne 0 ]]; then
-	    echo "Slide $i could not be found, assuming slide deck ended"
-	    break
-	fi
+    echo "Downloading slide $i"
+    curl "$BASE_LINK/$i" \
+	--silent \
+	--compressed \
+	--fail \
+	--output "${RANDOM_BASE_NAME}_$i.svg"
+    if [[ $? -ne 0 ]]; then
+	echo "Slide $i could not be found, assuming slide deck ended"
+	break
+    fi
 done
 
 echo "Download finished, converting and merging pdf"
